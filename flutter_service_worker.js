f0b26646e2a5da11ea5f431dbd8d6edda9a5d6e1
Matheus Ribeiro Miranda <43149218+matheusrmribeiro@users.noreply.github.com>
2020-04-15 21:38:55 -0300

    My portifolio

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,15 +4,15 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/LICENSE": "e7c0809f71a66c6690e0213699253fe9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "44b6747d76de72235ea529b0677316e6",
-"/": "44b6747d76de72235ea529b0677316e6",
-"main.dart.js": "dc0dc0c46314088a74a9bcd0bf9881ba",
-"manifest.json": "b94ad895711720dedf99eff7836484b9"
+"index.html": "47f4fd1bb72ba45629ee18e3059850e4",
+"/": "47f4fd1bb72ba45629ee18e3059850e4",
+"main.dart.js": "ae3f5f9796dd76f009a483bfbdfc4e00",
+"manifest.json": "1be9db6342f3c7d5d6ad928f61c39cf0"
 };
 
 self.addEventListener('activate', function (event) {
